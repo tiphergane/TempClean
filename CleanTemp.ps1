53724852893670361a4bd7954d1ba32f9d186bf3
tiphergane <tiphergane@gmail.com>
2019-01-02 14:08:31 +0100

    Initial commit

---
+++ CleanTemp.ps1
@@ -0,0 +1,35 @@
+﻿Set-ExecutionPolicy unrestricted
+######################################################################
+# Script vider les fichers temporaire
+# version: 1.0
+# Auteur: tiphergane / Meoowrwa
+# Synopsis: Effacer les différents répertoires de fichiers temporaire.
+# Usage: dans un powershell en mode admin, executer le script pour faire le ménage
+######################################################################
+
+function CleanTemp {
+    #Pour effacer le contenu du repertoire sans distinction d'extensions
+    del *
+}
+
+function testrep {
+
+    #On test si le répertoire existe
+    if (Test-Path -Path $repertoire)
+    {
+    #et si oui alors on le vide
+    Set-Location -Path $repertoire
+    CleanTemp
+    }
+    else
+    {
+    #si non, on affiche qu'il n'existe pas et on passe au suivant.
+    Write-Output "le repertoire $repertoire n'existe pas"
+    }
+}
+
+$repertoire = "C:\Windows\Temp"
+testrep
+$repertoire = "C:\$([Environment]::UserName)\Temp"
+testrep
+Remove-Variable repertoire
